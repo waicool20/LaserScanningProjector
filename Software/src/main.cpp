644697e42a184610bf9c60cb9dbb2c34520b09a6
waicool20 <waicool20@gmail.com>
2019-11-16 12:15:29 +0800

    Upload test code for various stuff

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -4,6 +4,9 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/spi.h>
 #include <drivers/st7735s.h>
+#include <drivers/laser.h>
+#include <algorithm>
+#include <drivers/laser_canvas.h>
 
 #include "drivers/stepper_motor.h"
 #include "lib/rcc.h"
@@ -11,6 +14,8 @@
 
 namespace {
 }  // namespace
+
+static constexpr double PI = 3.14159265;
 
 int main() {
   rcc::clock_setup_pll(rcc_hse8mhz_configs[RCC_CLOCK_HSE8_72MHZ]);
@@ -24,35 +29,81 @@ int main() {
   gpio down = gpio(GPIOC, GPIO13);
   gpio left = gpio(GPIOC, GPIO14);
   gpio right = gpio(GPIOB, GPIO8);
+  gpio center = gpio(GPIOB, GPIO9);
 
+  gpio ldr = gpio(GPIOB, GPIO0);
+
+  rcc_periph_clock_enable(RCC_GPIOA);
+
+  stepper_motor xM{gpio(GPIOB, GPIO7), gpio(GPIOB, GPIO6), gpio(GPIOB, GPIO5)};
+  stepper_motor yM{gpio(GPIOB, GPIO4), gpio(GPIOB, GPIO3), gpio(GPIOA, GPIO15)};
+  laser laser{};
 
   up.setup(GPIO_MODE_INPUT, GPIO_PUPD_PULLDOWN);
   down.setup(GPIO_MODE_INPUT, GPIO_PUPD_PULLDOWN);
   left.setup(GPIO_MODE_INPUT, GPIO_PUPD_PULLDOWN);
   right.setup(GPIO_MODE_INPUT, GPIO_PUPD_PULLDOWN);
+  center.setup(GPIO_MODE_INPUT, GPIO_PUPD_PULLDOWN);
+
+  laser_canvas canvas{25600, 128, 72, laser, xM, yM, ldr};
+
   lcd.draw_rect(1, 1, 32, 32, 0x00979D);
   lcd.draw_rect(1, lcd.get_height() - 33, 32, 32, 0x00979D);
   lcd.draw_rect(lcd.get_width() - 33, 1, 32, 32, 0x00979D);
   lcd.draw_rect(lcd.get_width() - 33, lcd.get_height() - 33, 32, 32, 0x00979D);
+
   uint8_t x = 0;
   uint8_t y = 0;
+  uint8_t dc = 50;
   while (true) {
     if (up.get()) {
       y -= 8;
-      lcd.draw_screen(0xFFFFFF);
+      yM.set_dir(stepper_motor::cw);
+      yM.do_steps(20);
     }
     if (down.get()) {
       y += 8;
-      lcd.draw_screen(0xFFFFFF);
+      yM.set_dir(stepper_motor::ccw);
+      yM.do_steps(20);
     }
     if (left.get()) {
       x -= 8;
-      lcd.draw_screen(0xFFFFFF);
+      xM.set_dir(stepper_motor::ccw);
+      xM.do_steps(20);
     }
     if (right.get()) {
       x += 8;
-      lcd.draw_screen(0xFFFFFF);
+      xM.set_dir(stepper_motor::cw);
+      xM.do_steps(20);
     }
-    lcd.draw_rect(lcd.get_width() / 2 + x, lcd.get_height() / 2 + y, 8, 8, 0x00);
+    if (center.get()) {
+      //canvas.home();
+      uint16_t deg = 0;
+      uint16_t centx = canvas.get_width() / 2;
+      uint16_t centy = canvas.get_height() / 2;
+      while(true) {
+/*        canvas.goto_xy(centx + 35 * cos(deg * PI / 180), centy + 35 * sin(deg * PI / 180));
+        if (++deg == 360) deg = 0;*/
+        for(uint32_t i = 0; i < canvas.get_height(); ++i) {
+          canvas.goto_xy(0, i);
+        }
+        systick::sleep_ms(5);
+        for(uint32_t i = 0; i < canvas.get_width(); ++i) {
+          canvas.goto_xy(i, canvas.get_height());
+        }
+        systick::sleep_ms(5);
+        for(uint32_t i = canvas.get_height(); i > 0; --i) {
+          canvas.goto_xy(canvas.get_width(), i);
+        }
+        systick::sleep_ms(5);
+        for(uint32_t i = canvas.get_width(); i > 0; --i) {
+          canvas.goto_xy(i, 0);
+        }
+        systick::sleep_ms(5);
+      }
+
+      //laser.toggle();
+    }
+    systick::sleep_ms(45);
   }
 }
