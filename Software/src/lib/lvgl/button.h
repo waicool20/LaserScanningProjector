0e13979bb79b535880137be7c2512a8ac617c632
waicool20 <waicool20@gmail.com>
2019-11-24 20:19:11 +0800

    Add handle constructor to button

--- Software/src/lib/lvgl/button.h
+++ Software/src/lib/lvgl/button.h
@@ -7,6 +7,7 @@ namespace lvgl {
 class button : public object {
  public:
   explicit button(object& parent, const button* copy = nullptr);
+  explicit button(lv_obj_t* handle);
 };
 
 }  // namespace lvgl
