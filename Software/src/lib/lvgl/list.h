6443bfafcb02ac909248b03971f4d930f546f5e9
waicool20 <waicool20@gmail.com>
2019-11-24 20:47:52 +0800

    Add missing return in list add_btn

--- Software/src/lib/lvgl/list.h
+++ Software/src/lib/lvgl/list.h
@@ -12,7 +12,7 @@ class list : public object {
   public:
     explicit list(lv_obj_t* parent = lv_scr_act(), const list* copy = nullptr);
     button add_btn(void* img, std::string_view txt);
-    button add_btn(std::string_view txt) { add_btn(nullptr, txt); }
+    button add_btn(std::string_view txt) { return add_btn(nullptr, txt); }
 };
 
 }
